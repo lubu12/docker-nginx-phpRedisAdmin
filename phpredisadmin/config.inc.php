b6779bfe277cb92f89ceaf3ea6859e66dfbdc469
EC2 Default User <ec2-user@ip-172-31-26-15.us-west-2.compute.internal>
2019-10-13 04:00:16 +0000

    good version

--- phpredisadmin/config.inc.php
+++ phpredisadmin/config.inc.php
@@ -1,127 +1,86 @@
 <?php
-require('/etc/phpmyadmin/config.secret.inc.php');
-/* Ensure we got the environment */
-$vars = array(
-    'PMA_ARBITRARY',
-    'PMA_HOST',
-    'PMA_HOSTS',
-    'PMA_VERBOSE',
-    'PMA_VERBOSES',
-    'PMA_PORT',
-    'PMA_PORTS',
-    'PMA_SOCKET',
-    'PMA_SOCKETS',
-    'PMA_USER',
-    'PMA_PASSWORD',
-    'PMA_ABSOLUTE_URI',
-    'PMA_CONTROLHOST',
-    'PMA_CONTROLPORT',
-    'PMA_PMADB',
-    'PMA_CONTROLUSER',
-    'PMA_CONTROLPASS',
-    'PMA_QUERYHISTORYDB'
+//Copy this file to config.inc.php and make changes to that file to customize your configuration.
+
+$config = array(
+  'servers' => array(
+    array(
+      'name' => 'redis - db0 - QA instance - Backend Cache/Session', // Optional name.
+      'host' => 'beta-magento-cache.ylv4lb.0001.usw2.cache.amazonaws.com',
+      'port' => 6379,
+      'filter' => '*',
+      'flush' => true,
+      'db'    => 0,
+      'databases' => 15,
+      'scheme' => 'tcp', // Optional. Connection scheme. 'tcp' - for TCP connection, 'unix' - for connection by unix domain socket
+      'path'   => '', // Optional. Path to unix domain socket. Uses only if 'scheme' => 'unix'. Example: '/var/run/redis/redis.sock'
+
+      // Optional Redis authentication.
+      //'auth' => 'redispasswordhere' // Warning: The password is sent in plain-text to the Redis server.
+    ),
+
+    /*array(
+      'host' => 'localhost',
+      'port' => 6380
+    ),*/
+
+    /*array(
+      'name'      => 'local db 2',
+      'host'      => 'localhost',
+      'port'      => 6379,
+      'db'        => 1,             // Optional database number, see http://redis.io/commands/select
+      'databases' => 1,             // Optional number of databases (prevents use of CONFIG command).
+      'filter'    => 'something:*', // Show only parts of database for speed or security reasons.
+      'seperator' => '/',           // Use a different seperator on this database (default uses config default).
+      'flush'     => false,         // Set to true to enable the flushdb button for this instance.
+      'charset'   => 'cp1251',      // Keys and values are stored in redis using this encoding (default utf-8).
+      'keys'      => false,         // Use the old KEYS command instead of SCAN to fetch all keys for this server (default uses config default).
+      'scansize'  => 1000           // How many entries to fetch using each SCAN command for this server (default uses config default).
+    ),*/
+  ),
+
+
+  'seperator' => ':',
+
+
+  // Uncomment to show less information and make phpRedisAdmin fire less commands to the Redis server. Recommended for a really busy Redis server.
+  //'faster' => true,
+
+
+  // Uncomment to enable HTTP authentication
+  /*'login' => array(
+    // Username => Password
+    // Multiple combinations can be used
+    'admin' => array(
+      'password' => 'adminpassword',
+    ),
+    'guest' => array(
+      'password' => '',
+      'servers'  => array(1) // Optional list of servers this user can access.
+    )
+  ),*/
+
+  // Use HTML form/cookie-based auth instead of HTTP Basic/Digest auth
+  'cookie_auth' => false,
+
+
+  /*'serialization' => array(
+    'foo*' => array( // Match like KEYS
+      // Function called when saving to redis.
+      'save' => function($data) { return json_encode(json_decode($data)); },
+      // Function called when loading from redis.
+      'load' => function($data) { return json_encode(json_decode($data), JSON_PRETTY_PRINT); },
+    ),
+  ),*/
+
+
+  // You can ignore settings below this point.
+
+  'maxkeylen'           => 100,
+  'count_elements_page' => 100,
+
+  // Use the old KEYS command instead of SCAN to fetch all keys.
+  'keys' => false,
+
+  // How many entries to fetch using each SCAN command.
+  'scansize' => 1000
 );
-foreach ($vars as $var) {
-    $env = getenv($var);
-    if (!isset($_ENV[$var]) && $env !== false) {
-        $_ENV[$var] = $env;
-    }
-}
-if (isset($_ENV['PMA_QUERYHISTORYDB'])) {
-    $cfg['QueryHistoryDB'] = boolval($_ENV['PMA_QUERYHISTORYDB']);
-}
-/* Arbitrary server connection */
-if (isset($_ENV['PMA_ARBITRARY']) && $_ENV['PMA_ARBITRARY'] === '1') {
-    $cfg['AllowArbitraryServer'] = true;
-}
-/* Play nice behind reverse proxys */
-if (isset($_ENV['PMA_ABSOLUTE_URI'])) {
-    $cfg['PmaAbsoluteUri'] = trim($_ENV['PMA_ABSOLUTE_URI']);
-}
-/* Figure out hosts */
-/* Fallback to default linked */
-$hosts = array('db');
-/* Set by environment */
-if (!empty($_ENV['PMA_HOST'])) {
-    $hosts = array($_ENV['PMA_HOST']);
-    $verbose = array($_ENV['PMA_VERBOSE']);
-    $ports = array($_ENV['PMA_PORT']);
-} elseif (!empty($_ENV['PMA_HOSTS'])) {
-    $hosts = array_map('trim', explode(',', $_ENV['PMA_HOSTS']));
-    $verbose = array_map('trim', explode(',', $_ENV['PMA_VERBOSES']));
-    $ports = array_map('trim', explode(',', $_ENV['PMA_PORTS']));
-}
-if (!empty($_ENV['PMA_SOCKET'])) {
-    $sockets = array($_ENV['PMA_SOCKET']);
-} elseif (!empty($_ENV['PMA_SOCKETS'])) {
-    $sockets = explode(',', $_ENV['PMA_SOCKETS']);
-}
-/* Server settings */
-for ($i = 1; isset($hosts[$i - 1]); $i++) {
-    $cfg['Servers'][$i]['host'] = $hosts[$i - 1];
-    if (isset($verbose[$i - 1])) {
-        $cfg['Servers'][$i]['verbose'] = $verbose[$i - 1];
-    }
-    if (isset($ports[$i - 1])) {
-        $cfg['Servers'][$i]['port'] = $ports[$i - 1];
-    }
-    if (isset($_ENV['PMA_USER'])) {
-        $cfg['Servers'][$i]['auth_type'] = 'config';
-        $cfg['Servers'][$i]['user'] = $_ENV['PMA_USER'];
-        $cfg['Servers'][$i]['password'] = isset($_ENV['PMA_PASSWORD']) ? $_ENV['PMA_PASSWORD'] : '';
-    } else {
-        $cfg['Servers'][$i]['auth_type'] = 'cookie';
-    }
-    if (isset($_ENV['PMA_PMADB'])) {
-      $cfg['Servers'][$i]['pmadb'] = $_ENV['PMA_PMADB'];
-      $cfg['Servers'][$i]['relation'] = 'pma__relation';
-      $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-      $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-      $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-      $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-      $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-      $cfg['Servers'][$i]['history'] = 'pma__history';
-      $cfg['Servers'][$i]['recent'] = 'pma__recent';
-      $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-      $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-      $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-      $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-      $cfg['Servers'][$i]['users'] = 'pma__users';
-      $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-      $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-      $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-      $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-      $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-      $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
-    }
-    if (isset($_ENV['PMA_CONTROLHOST'])) {
-      $cfg['Servers'][$i]['controlhost'] = $_ENV['PMA_CONTROLHOST'];
-    }
-    if (isset($_ENV['PMA_CONTROLPORT'])) {
-      $cfg['Servers'][$i]['controlport'] = $_ENV['PMA_CONTROLPORT'];
-    }
-    if (isset($_ENV['PMA_CONTROLUSER'])) {
-      $cfg['Servers'][$i]['controluser'] = $_ENV['PMA_CONTROLUSER'];
-    }
-    if (isset($_ENV['PMA_CONTROLPASS'])) {
-      $cfg['Servers'][$i]['controlpass'] = $_ENV['PMA_CONTROLPASS'];
-    }
-    $cfg['Servers'][$i]['compress'] = false;
-    $cfg['Servers'][$i]['AllowNoPassword'] = true;
-}
-for ($i = 1; isset($sockets[$i - 1]); $i++) {
-    $cfg['Servers'][$i]['socket'] = $sockets[$i - 1];
-    $cfg['Servers'][$i]['host'] = 'localhost';
-}
-/*
- * Revert back to last configured server to make
- * it easier in config.user.inc.php
- */
-$i--;
-/* Uploads setup */
-$cfg['UploadDir'] = '';
-$cfg['SaveDir'] = '';
-/* Include User Defined Settings Hook */
-if (file_exists('/etc/phpmyadmin/config.user.inc.php')) {
-    include('/etc/phpmyadmin/config.user.inc.php');
-}
--- phpredisadmin/docker-entrypoint.sh
+++ phpredisadmin/docker-entrypoint.sh
@@ -1,52 +1,9 @@
 #!/bin/bash
-if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
-    if [ "$(id -u)" = '0' ]; then
-        case "$1" in
-            apache2*)
-                user="${APACHE_RUN_USER:-www-data}"
-                group="${APACHE_RUN_GROUP:-www-data}"
-                ;;
-            *) # php-fpm
-                user='app'
-                group='app'
-                ;;
-        esac
-    else
-        user="$(id -u)"
-        group="$(id -g)"
-    fi
-
-    chown app:app /sessions /var/nginx/client_body_temp
-    mkdir /var/www/html/phpMyAdmin
-    cd /var/www/html/phpMyAdmin
-
-    if ! [ -e index.php -a -e db_designer.php ]; then
-        echo >&2 "phpMyAdmin not found in $PWD - copying now..."
-        if [ "$(ls -A)" ]; then
-            echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
-            ( set -x; ls -A; sleep 10 )
-        fi
-        tar --create \
-            --file - \
-            --one-file-system \
-            --directory /usr/src/phpmyadmin \
-            --owner "$user" --group "$group" \
-            . | tar --extract --file -
-        echo >&2 "Complete! phpMyAdmin has been successfully copied to $PWD"
-        mkdir -p tmp; \
-        chmod -R 777 tmp; \
-    fi
-
-    if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
-        cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
-<?php
-\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
-EOT
-    fi
-
-    if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
-        touch /etc/phpmyadmin/config.user.inc.php
-    fi
-fi
+git clone https://github.com/ErikDubbelboer/phpRedisAdmin.git
+cd phpRedisAdmin
+git clone https://github.com/nrk/predis.git vendor
+cp /tmp/config.inc.php includes/
+rm /tmp/config.inc.php
+chown -R app:app *
 
 exec "$@"
